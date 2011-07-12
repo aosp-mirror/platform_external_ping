e9640ed4c9c4441b512fd17344b8ca7ab507c176
Jeff Brown <jeffbrown@google.com>
2011-07-11 22:12:04 -0700

    Remove the simulator target from all makefiles. Bug: 5010576
    Change-Id: I5b5fa17d53d2d013297858bb9c8b789b0297b7b7

--- Android.mk
+++ Android.mk
@@ -1,5 +1,3 @@
-ifneq ($(TARGET_SIMULATOR),true)
-
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -7,5 +5,3 @@ LOCAL_SRC_FILES:= ping.c
 LOCAL_MODULE := ping
 LOCAL_STATIC_LIBRARIES := libcutils libc
 include $(BUILD_EXECUTABLE)
-
-endif  # TARGET_SIMULATOR != true
